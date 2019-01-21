564182f65b999236f86d40f138de7d1cf7f4d71a
mikezks <michael.zikes@intauria.com>
2019-01-21 13:55:50 +0100

    First Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,22 +12,28 @@ import {
   MatSnackBarModule,
   MatToolbarModule
 } from '@angular/material';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { HomeComponent } from './home/home.component';
+import { FlightSearchComponent } from './flight-search/flight-search.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
     HomeComponent,
+    FlightSearchComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    HttpClientModule,
+    FormsModule,
     LayoutModule,
     MatButtonModule,
     MatCardModule,
